8f77f37ac6df6f6fcfbe1b62406ddf001e4a51f5
Alex Bikfalvi <alex@bikfalvi.com>
2013-09-29 22:26:00 +0200

    Initial commit.

---
+++ CloudLock/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CloudLock
+{
+	/// <summary>
+	/// A class representing the main form.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		/// <summary>
+		/// Creates a new form instance.
+		/// </summary>
+		public MainForm()
+		{
+			// Initialize the component.
+			this.InitializeComponent();
+		}
+	}
+}
